44a84947cafb643a8cda69d36623f8f9440e4441
Shubham Shaw <shaw.mywork@gmail.com>
2026-06-26 22:51:55 +0530

    Add files via upload

---
+++ AND_GATE/and_gate.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11.06.2026 00:58:28
+// Design Name: 
+// Module Name: and_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module and_gate(
+    input B,
+    output Y
+    );
+endmodule
