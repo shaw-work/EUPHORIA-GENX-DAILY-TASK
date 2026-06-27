5c29599276af76917a9e82fbe7728eb1e577a54f
Shubham Shaw <shaw.mywork@gmail.com>
2026-06-27 22:59:14 +0530

    Add files via upload

---
+++ HALF_ADDER-GATE/half_adder_tb.v
@@ -0,0 +1,48 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity half_adder_tb is
+end half_adder_tb;
+
+architecture Behavioral of half_adder_tb is
+
+    component half_adder
+        Port (
+            A    : in  STD_LOGIC;
+            B    : in  STD_LOGIC;
+            SUM  : out STD_LOGIC;
+            CARRY: out STD_LOGIC
+        );
+    end component;
+
+    signal A, B : STD_LOGIC := '0';
+    signal SUM, CARRY : STD_LOGIC;
+
+begin
+
+    UUT: half_adder
+        port map (
+            A => A,
+            B => B,
+            SUM => SUM,
+            CARRY => CARRY
+        );
+
+    stim_proc: process
+    begin
+        A <= '0'; B <= '0';
+        wait for 10 ns;
+
+        A <= '0'; B <= '1';
+        wait for 10 ns;
+
+        A <= '1'; B <= '0';
+        wait for 10 ns;
+
+        A <= '1'; B <= '1';
+        wait for 10 ns;
+
+        wait;
+    end process;
+
+end Behavioral;
